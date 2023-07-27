cb815993e3966196f8f9a725efdb04d56f7ddbe7
TolgaARSLANN <tolgaarslan@ogr.bandirma.edu.tr>
2023-07-28 00:44:22 +0300

    Edit

--- python-kursu-master/src/2-myAssignment/myProject.py
+++ python-kursu-master/src/2-myAssignment/myProject.py
@@ -3,5 +3,6 @@ soyad = "ARSLAN"
 yas = 12
 cinsiyet = "kız"
 boy = 1.58
+job = "student"
 
-print(f"Bneim adım{ad} soyadım {soyad} {yas} yaşındayım boyum {boy} ve cisniyetim {cinsiyet}")
+print(f"Bneim adım{ad} soyadım {soyad} {yas} yaşındayım boyum {boy} ve cisniyetim {cinsiyet} ayrıca mesleğim {job}")
