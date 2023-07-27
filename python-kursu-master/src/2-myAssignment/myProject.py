28e948c4611ada9d8603e64d1ed027b49e484694
TolgaARSLANN <tolgaarslan@ogr.bandirma.edu.tr>
2023-07-28 00:24:51 +0300

    Latest Version

---
+++ python-kursu-master/src/2-myAssignment/myProject.py
@@ -0,0 +1,7 @@
+ad = "cemre"
+soyad = "ARSLAN"
+yas = 12
+cinsiyet = "kız"
+boy = 1.58
+
+print(f"Bneim adım{ad} soyadım {soyad} {yas} yaşındayım boyum {boy} ve cisniyetim {cinsiyet}")
---
+++ python-kursu-master/src/7-Pythonda Fonksiyonlar/scope.py
@@ -0,0 +1,53 @@
+# # global scope
+# x = 'global x'
+
+# def function(): 
+#     # local scope
+#     # x = 'local x'
+#     print(x)
+
+# function()
+# print(x)
+
+####################
+
+# global
+# name = 'Çinar'
+
+# def changeName(new_name):
+#     # local variable scope 
+#     global name
+#     name = new_name
+#     print(name)
+
+# changeName('Ada')
+# print(name)
+
+# ####################
+
+# name = 'global string'
+
+# def greeting():
+#     # name = 'Çinar'
+
+#     def hello():
+#         # name = 'Ada'
+#         print('hello '+ name)
+
+#     hello()
+
+# greeting()
+
+# ####################
+
+x = 50
+def test(): 
+    global x
+    print(f'x : {x}')
+
+    x = 100
+    print(f'changed x to {x}')
+
+test()
+print(x)
+
