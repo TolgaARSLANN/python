e5fa6dc3f803f7defc40624715f13ad1bf1c0a30
TolgaARSLANN <tolgaarslan@ogr.bandirma.edu.tr>
2023-07-28 14:05:09 +0300

    Last Edit

--- python-kursu-master/src/7-Pythonda Fonksiyonlar/function-demo.py
+++ python-kursu-master/src/7-Pythonda Fonksiyonlar/function-demo.py
@@ -23,33 +23,33 @@
 
 # 3- Gönderilen 2 sayı arasındaki tüm asal sayıları bulun.
 
-# def asalSayılariBul(sayi1, sayi2):
-#     for sayi in range(sayi1, sayi2+1):
-#         if sayi > 1:
-#             for i in range(2, sayi):
-#                 if (sayi % i == 0):
-#                     break
-#             else:
-#                 print(sayi)
+def asalSayılariBul(sayi1, sayi2):
+    for sayi in range(sayi1, sayi2+1):
+        if sayi > 1:
+            for i in range(2, sayi):
+                if (sayi % i == 0):
+                    break
+            else:
+                print(sayi)
 
-# sayi1 = int(input('sayı 1:'))
-# sayi2 = int(input('sayı 2:'))
+sayi1 = int(input('sayı 1:'))
+sayi2 = int(input('sayı 2:'))
 
-# asalSayılariBul(sayi1, sayi2)
+asalSayılariBul(sayi1, sayi2)
 
 
 
 # 4- Kendisine gönderilen bir sayının tam bölenlerini bir liste şeklinde döndürünüz.
 
 
-def tamBolenleriBul(sayi):
-    tamBolenler = []
+# def tamBolenleriBul(sayi):
+#     tamBolenler = []
 
-    for i in range(2, sayi):
-        if (sayi % i == 0):
-            tamBolenler.append(i)
+#     for i in range(2, sayi):
+#         if (sayi % i == 0):
+#             tamBolenler.append(i)
     
-    return tamBolenler
+#     return tamBolenler
 
 
-print(tamBolenleriBul(20))
+# print(tamBolenleriBul(20))
